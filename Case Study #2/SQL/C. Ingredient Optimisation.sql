4009a2609ec1554e965954e4d345e7a20a6a3036
Marcelle Mvondo <am.mvondo@gmail.com>
2023-04-24 12:40:40 +0100

    Case #3 start

--- Case Study #2/SQL/C. Ingredient Optimisation.sql
+++ Case Study #2/SQL/C. Ingredient Optimisation.sql
@@ -22,3 +22,33 @@ GROUP BY topping_name
 ORDER BY COUNT(topping_name) DESC
 LIMIT 1
 
+-- 3. What was the most common exclusion?
+WITH exclusion_table AS 
+(
+	SELECT UNNEST(STRING_TO_ARRAY(exclusions, ',') :: int []) AS topping_id		
+	FROM _customer_orders AS c
+	WHERE exclusions != 'null'       
+)
+SELECT topping_name,COUNT(topping_name) AS nb_of_pizzas    
+FROM exclusion_table AS et
+JOIN pizza_toppings AS t 
+  	ON et.topping_id = t.topping_id
+GROUP BY topping_name
+ORDER BY COUNT(topping_name) DESC
+LIMIT 1
+
+/* --------------------
+4. Generate an order item for each record in the customers_orders table in the format of one of the following:
+Meat Lovers
+Meat Lovers - Exclude Beef
+Meat Lovers - Extra Bacon
+Meat Lovers - Exclude Cheese, Bacon - Extra Mushroom, Peppers
+   --------------------*/
+   
+   
+/* --------------------
+5. Generate an alphabetically ordered comma separated ingredient list for each pizza order from the customer_orders table and add a 2x in front of any relevant ingredients
+For example: "Meat Lovers: 2xBacon, Beef, ... , Salami"
+   --------------------*/
+   
+-- 6. What is the total quantity of each ingredient used in all delivered pizzas sorted by most frequent first?
